bc34ccf03306a7ba9a5f529c3c7cb952196e2a19
Renan Huanca <renan.huanca@mojix.com>
2017-09-23 01:09:25 -0400

    work in progress

--- example2/lightsensor/lightsensor.ino
+++ example2/lightsensor/lightsensor.ino
@@ -4,11 +4,13 @@
 int photocellPin = 0;
 int photocellReading;
 int LEDpin = 11;
-int LEDbrightness;  
+int sequenceNumber = 1;
+unsigned long time;   
 
 byte mac[] = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 char topic[] = "/v1/test";
-char broker_id[] = "client1";
+char brokerId[] = "client1";
+char serialNumber[] = "TESTTAG0001";
 
 IPAddress ip(10, 42, 0, 3);
 IPAddress server(34,240,137,175);
@@ -25,7 +27,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
 void setup() {
   Serial.begin(9600);
   Ethernet.begin(mac, ip);   
-  if (client.connect(broker_id)) {
+  if (client.connect(brokerId)) {
     Serial.println("Connected to MQTT broker");
   } else {
     Serial.println("Unable to connect to MQTT broker");
@@ -33,18 +35,29 @@ void setup() {
 }
  
 void loop() {
+  time = millis();
   photocellReading = analogRead(photocellPin);  
   photocellReading = 1023 - photocellReading;
 
+  String msg = "";
+  msg = msg + "sn," + sequenceNumber + "\n";
+  
   if(photocellReading<100) {
-    client.publish(topic,"hello world"); 
+    msg = msg + serialNumber + ","+time+",status,ON";
   } else {
-    client.publish(topic,"NOT hello world");
+    msg = msg + serialNumber + ","+time+",status,OFF";
   }
+
+  char payload[msg.length()+1];
+  msg.toCharArray(payload, msg.length()+1);
+
+  client.publish(topic, payload);
 
   Serial.print("reading: ");
   Serial.println(photocellReading);    
+  Serial.println(payload);
   
   delay(2000);
+  sequenceNumber = sequenceNumber + 1;
   client.loop();
 }
