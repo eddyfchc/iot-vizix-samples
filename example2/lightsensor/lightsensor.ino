ae70a399483967f9b33e7afdf175dd41fb798726
Renan Huanca <renan.huanca@mojix.com>
2017-09-23 03:11:02 -0400

    added time support

--- example2/lightsensor/lightsensor.ino
+++ example2/lightsensor/lightsensor.ino
@@ -1,12 +1,16 @@
 #include <Ethernet.h>
 #include <PubSubClient.h>
+
+#include <TimeLib.h>
+
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
  
 int photocellPin = 0;
 int photocellReading;
 int LEDpin = 11;
 int sequenceNumber = 1;
 unsigned long time;  
-unsigned long startTime = 1506147865; 
 
 byte mac[] = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 char topic[] = "/v1/test";
@@ -27,6 +31,10 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial) ; // Needed for Leonardo only
+  setSyncProvider( requestSync);  //set function to call when sync required
+  Serial.println("Waiting for sync message");
+  
   Ethernet.begin(mac, ip);   
   if (client.connect(brokerId)) {
     Serial.println("Connected to MQTT broker");
@@ -36,7 +44,11 @@ void setup() {
 }
  
 void loop() {
-  time = startTime+ millis();
+  if (Serial.available()) {
+    processSyncMessage();
+  }
+
+  time = now();
   photocellReading = analogRead(photocellPin);  
   photocellReading = 1023 - photocellReading;
 
@@ -44,9 +56,9 @@ void loop() {
   msg = msg + "sn," + sequenceNumber + "\n";
   
   if(photocellReading<100) {
-    msg = msg + serialNumber + ","+time+",status,ON";
+    msg = msg + serialNumber + ","+time+"000,status,ON";
   } else {
-    msg = msg + serialNumber + ","+time+",status,OFF";
+    msg = msg + serialNumber + ","+time+"000,status,OFF";
   }
 
   char payload[msg.length()+1];
@@ -63,3 +75,21 @@ void loop() {
   client.loop();
 }
 
+void processSyncMessage() {
+  unsigned long pctime;
+  const unsigned long DEFAULT_TIME = 1357041600; // Jan 1 2013
+
+  if(Serial.find(TIME_HEADER)) {
+     pctime = Serial.parseInt();
+     
+     if( pctime >= DEFAULT_TIME) { // check the integer is a valid time (greater than Jan 1 2013)
+       setTime(pctime); // Sync Arduino clock to the time received on the serial port
+     }
+  }
+}
+
+time_t requestSync()
+{
+  Serial.write(TIME_REQUEST);  
+  return 0; // the time will be sent later in response to serial mesg
+}
